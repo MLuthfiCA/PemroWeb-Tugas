945f32131b9723d684932713de8f5214ddc5e2e4
MLuthfiCA <lutfiii132000@gmail.com>
2026-03-15 22:47:20 +0700

    Add route for items index using ItemController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/items', [ItemController::class, 'index'])->name('items.index');
