c700b1157fbfe0df0a1e89528e9cfc830e61cfde
MLuthfiCA <lutfiii132000@gmail.com>
2026-04-03 16:19:39 +0700

    Add new routes for katalog, riwayat, and pengajuan

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/items', [ItemController::class, 'index'])->name('items.index');
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/katalog', function () {
+    return view('pages.katalog'); 
+});
+
+Route::get('/riwayat', function () {
+    return view('pages.riwayat');
+});
+
+Route::get('/pengajuan', function () {
+    return view('pages.form_pengajuan');
+});
