fba0d1f8b57d279bffab515644eadb697def5bcf
Kane-98 <riskykurnia0812@gmail.com>
2026-04-06 08:51:35 +0100

    Add login route to web.php

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\RiwayatController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
 Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::get('/riwayat', [RiwayatController::class, 'tampilkanRiwayat']);
+Route::get('/login', [LoginController::class, 'login']);
 
 // Tambahan untuk sidebar
 Route::get('/katalog', function () {
