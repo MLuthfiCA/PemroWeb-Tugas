70366b3fbfb0118ad57e80a10d9cf86e01a20d61
zahrahathirah2605-ops <zahrah.athirah2605@gmail.com>
2026-04-06 09:17:48 +0700

    login page

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,23 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\RiwayatController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
 Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/riwayat', [RiwayatController::class, 'tampilkanRiwayat']);
 
 // Tambahan untuk sidebar
+
+Route::get('/login', function () {
+    return view('login');
+});
+
 Route::get('/katalog', function () {
     return view('katalog');
 });
 
-Route::get('/riwayat', function () {
-    return 'Halaman Riwayat';
-});
 
 Route::get('/pengajuan', function () {
     return view('pengajuan');
