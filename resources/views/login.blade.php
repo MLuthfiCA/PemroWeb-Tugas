80d278e37d77a8a26029f9dc9607b59e6ea926f1
Kane-98 <riskykurnia0812@gmail.com>
2026-04-06 13:12:51 +0100

    Update login view to use asset helper for paths
    Sedikit perbaikan kodingan pada login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,16 +1,15 @@
-<?
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="CSS/login.blade.css">
+    <link rel="stylesheet" href="{{ asset('styles/login.css') }}">
 </head>
 <body>
 <div class="container">
     <center>
-        <img src="images/poltek.png" class="form-img">
+        <img src="{{ asset('images/poltek.png') }}" class="form-img">
     </center>
     <h2>System Perpustakaan Digital</h2>
     <form action="#" class="form">
@@ -23,4 +22,3 @@
 </div>
 </body>
 </html>
-?>
