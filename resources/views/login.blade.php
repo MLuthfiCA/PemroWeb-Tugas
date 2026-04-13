36b93c149aacc6c18ef346b1c18e6a7378faecc9
MLuthfiCA <lutfiii132000@gmail.com>
2026-04-13 10:06:25 +0700

    update logn

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,11 +9,11 @@
 <body class="bg-gray-100">
     <div class="w-1/2 mx-auto mt-6 bg-white rounded-lg shadow-md p-4">
         <div class="flex justify-center">
-            <img src="{{ asset('images/poltek.png') }}" class="w-[260px] h-[260px] pt-5">
+            <img src="{{ asset('images/readspace-library.png') }}" class="w-[260px] h-[260px] pt-5">
         </div>
 
         <h2 class="text-center text-blue-600 font-bold text-xl py-4">
-            Sistem Perpustakaan Digital
+            ReadSpace Library 
         </h2>
 
         <form class="max-w-xl mx-auto flex flex-col">
@@ -25,10 +25,22 @@
             <input type="password"
                 placeholder="Enter your password"
                 class="mb-4 p-3 text-base border border-gray-300 rounded focus:outline-none focus:border-blue-500">
+                        <form class="max-w-xl mx-auto flex flex-col">
+            <label class="py-2">Login Sebagai:</label>
+            <select id="role" name="role" required
+                class="mb-4 p-3 text-base border border-gray-300 rounded bg-white focus:outline-none focus:border-blue-500">
+                <option value="" disabled selected>Choose User</option>
+                <option value="mahasiswa">User</option>
+                <option value="admin">Admin</option>
+            </select>
             <button
-                class="bg-red-500 text-white py-2 px-4 mb-4 border rounded text-lg hover:bg-red-600 transition">
+                        primary: '#632024'
+                class="bg-[#632024] text-white py-2 px-4 mb-4 border rounded text-lg hover:bg-red-600 transition">
                 Log In
             </button>
+            <p class="text-center text-sm text-gray-600 mt-2">
+    Belum ada akun? <a href="{{ route('register') }}" class="text-red-600 font-semibold hover:underline">Daftar</a>
+            </p>
         </form>
     </div>
 </body>
